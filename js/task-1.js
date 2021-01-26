2e0593ddcc36fed32f38d83e77a313fe653d8c67
Yuliia Burko <yuliia.burko@gmail.com>
2021-01-26 10:53:20 +0100

    Правки ДЗ 07

--- js/task-1.js
+++ js/task-1.js
@@ -3,11 +3,11 @@
 const liRef = document.querySelectorAll('.item');
 console.log(`В списке ${liRef.length} категории`);
 
-const liArray = [...liRef]
-  .map(
-    liElem => `Категория: ${liElem.children[0].textContent}
-Количество элементов: ${liElem.children[1].children.length}`,
-  )
-  .join('\n');
-
-console.log(liArray);
+const itemsRef = document.querySelectorAll('.item');
+Array.prototype.forEach.call(itemsRef, element => {
+  const titleRef = element.querySelector('h2').innerHTML;
+  const itemsLengthRef = element.querySelectorAll('li').length;
+  console.log(
+    `Категория: ${titleRef}\nКоличество элементов: ${itemsLengthRef}`,
+  );
+});
--- js/task-6.js
+++ js/task-6.js
@@ -1,12 +1,16 @@
 'use strict';
 
 const inputRef = document.querySelector('#validation-input');
+const inputLengthRef = inputRef.getAttribute('data-length');
 
 inputRef.addEventListener('blur', () => {
-  if (inputRef.value.length < 6) {
+  if (
+    inputRef.value.length < inputLengthRef ||
+    inputRef.value.length > inputLengthRef
+  ) {
     inputRef.classList.add('invalid');
   }
-  if (inputRef.value.length >= 6) {
+  if (inputRef.value.length == inputLengthRef) {
     inputRef.classList.remove('invalid');
     inputRef.classList.add('valid');
   }
